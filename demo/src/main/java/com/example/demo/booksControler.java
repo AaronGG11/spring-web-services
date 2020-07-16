213341404e6acf7e33d1af2c979e7bdb4e8f3471
AaronGG11 <AaronGG11>
2020-07-16 16:26:37 -0500

    Creando un controlador simple REST

---
+++ demo/src/main/java/com/example/demo/booksControler.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class booksControler {
+	@GetMapping("/books")
+	public List<book> getAllBooks(){
+		return Arrays.asList(
+				new book(1l,"Soy sola", "Pedro Sola"));
+	}
+	
+}
