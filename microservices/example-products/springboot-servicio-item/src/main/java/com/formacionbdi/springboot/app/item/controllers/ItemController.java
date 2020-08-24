2eecbe22824574e601645bf78e542c44fde9cd33
AaronGG11 <AaronGG11>
2020-08-24 12:24:43 -0500

    Adding new profiles in configuration files

--- microservices/example-products/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ microservices/example-products/springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +23,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	
 	@Autowired
 	@Qualifier("serviceRestTemplate") // es el nombre de la clase en minuscula inicial
 	private ItemService itemService; // ahora tenemos 2 servicio que implementan a la interfaz
+	
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -35,7 +50,7 @@ public class ItemController {
 	// CAMINO ALTERNATIVO QUE EVITA ERRORES EN CASCADA 
 	public Item metodoAlternativo(Long id, Integer cantidad)
 	{
-		Item item = new Item();
+		Item item = new Item(); 
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
 		producto.setId(id);
@@ -44,6 +59,19 @@ public class ItemController {
 		item.setProducto(producto);
 		
 		return item;
+	} 
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto)
+	{
+		log.info(texto);
+		
+		Map<String,String> json = new HashMap<>();
+		json.put("text", texto);
+		json.put("puerto", puerto);
+		
+		return new ResponseEntity<Map<String,String>>(json,HttpStatus.OK);
 	}
+			
 
 }
